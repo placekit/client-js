1cc64f2ee8c87094474acba1f60214a5907d72b4
noclat <noclat@gmail.com>
2022-07-23 10:03:20 +0200

    add license and banner

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,43 @@
 import commonjs from '@rollup/plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
 
+import packageJSON from './package.json';
+const banner = [
+  `/*! PlaceKit v${packageJSON.version}`,
+  '© PlaceKit.io',
+  'MIT License',
+  `${packageJSON.homepage} */`,
+].join(' | ');
+
 export default {
   input: 'src/index.js',
   output: [
     {
       file: 'dist/index.esm.js',
       format: 'es',
+      banner,
     },
     {
       file: 'dist/index.cjs.js',
       format: 'cjs',
       exports: 'auto',
+      banner,
     },
     {
       file: 'dist/index.umd.js',
       format: 'umd',
       name: 'PlaceKit',
-      plugins: [terser()],
+      plugins: [
+        terser({
+          format: {
+            preamble: banner,
+            comments: false,
+          }
+        }),
+      ],
     },
   ],
+  external: [/node_modules/],
   plugins: [
     commonjs(),
   ],
