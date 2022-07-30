b25d38316548b8f65bbc370812bee176fdb88c33
noclat <noclat@gmail.com>
2022-07-30 14:04:03 +0200

    minor changes

--- examples/node-typescript/index.ts
+++ examples/node-typescript/index.ts
@@ -1,5 +1,5 @@
 import dotenv from 'dotenv';
-dotenv.config({});
+dotenv.config();
 
 import PlaceKit from '../../';
 import type { PKInstance, PKOptions, PKResponse } from '../../';
--- examples/node/index.js
+++ examples/node/index.js
@@ -1,4 +1,4 @@
-require('dotenv').config({});
+require('dotenv').config();
 const placekit = require('../../');
 
 const pkSearch = placekit({
