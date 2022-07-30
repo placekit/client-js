2eccfdcb56c9c3254a98f48d858e369cc1f8555c
noclat <noclat@gmail.com>
2022-07-30 10:42:54 +0200

    fix namespace types export

--- examples/node-typescript/index.ts
+++ examples/node-typescript/index.ts
@@ -2,12 +2,13 @@ import dotenv from 'dotenv';
 dotenv.config({});
 
 import PlaceKit from '../../';
+import type { PKInstance, PKOptions, PKResponse } from '../../';
 
-const options: Partial<PlaceKit.PKOptions> = {
+const options: Partial<PKOptions> = {
   hitsPerPage: 5,
 };
 
-const pkSearch: PlaceKit.PKInstance = PlaceKit({
+const pkSearch: PKInstance = PlaceKit({
   appId: `${process.env.PLACEKIT_APP_ID}`,
   apiKey: `${process.env.PLACEKIT_API_KEY}`,
   options,
@@ -16,6 +17,6 @@ const pkSearch: PlaceKit.PKInstance = PlaceKit({
 const [query] = process.argv.slice(2);
 
 (async () => {
-  const res: PlaceKit.PKResponse = await pkSearch(query || '');
+  const res: PKResponse = await pkSearch(query || '');
   console.log(res);
 })();
--- src/index.d.ts
+++ src/index.d.ts
@@ -4,35 +4,33 @@ export as namespace PlaceKit;
 declare function PlaceKit({ appId, apiKey, options }?: {
   appId: string;
   apiKey: string;
-  options: Partial<PlaceKit.PKOptions>;
-}): PlaceKit.PKInstance;
+  options: Partial<PKOptions>;
+}): PKInstance;
 
-declare namespace PlaceKit {
-  export interface PKInstance {
-    (query: string, opts?: Partial<PKOptions>): Promise<PKResponse>;
-    readonly options: PKOptions;
-    configure(opts?: Partial<PKOptions>): void;
-    readonly hasGeolocation: boolean;
-    requestGeolocation(timeout?: number): Promise<GeolocationPosition>;
-  }
-
-  export type PKOptions = {
-    retryTimeout: number;
-    language: string;
-    countries: string[];
-    type: string;
-    hitsPerPage: number;
-    postcodeSearch: boolean;
-    aroundLatLng: string;
-    aroundLatLngViaIP: boolean;
-    aroundRadius: number;
-    insideBoundingBox: string;
-    insidePolygon: string;
-    getRankingInfo: string;
-    computeQueryParams: any;
-  };
-
-  export type PKResponse = {
-    hits: any[];
-  };
+export interface PKInstance {
+  (query: string, opts?: Partial<PKOptions>): Promise<PKResponse>;
+  readonly options: PKOptions;
+  configure(opts?: Partial<PKOptions>): void;
+  readonly hasGeolocation: boolean;
+  requestGeolocation(timeout?: number): Promise<GeolocationPosition>;
 }
+
+export type PKOptions = {
+  retryTimeout: number;
+  language: string;
+  countries: string[];
+  type: string;
+  hitsPerPage: number;
+  postcodeSearch: boolean;
+  aroundLatLng: string;
+  aroundLatLngViaIP: boolean;
+  aroundRadius: number;
+  insideBoundingBox: string;
+  insidePolygon: string;
+  getRankingInfo: string;
+  computeQueryParams: any;
+};
+
+export type PKResponse = {
+  hits: any[];
+};
