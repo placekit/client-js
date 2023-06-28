f7fd2b64446691438f0cb82f7a5f9da86e81a089
noclat <noclat@gmail.com>
2023-06-28 09:57:15 +0200

    feat:split client into lite and full versions

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,3 @@
-import path from 'node:path';
-
-import commonjs from '@rollup/plugin-commonjs';
 import cleanup from 'rollup-plugin-cleanup';
 import copy from 'rollup-plugin-copy';
 
@@ -12,39 +9,57 @@ const banner = [
   `${pkg.homepage} */`,
 ].join(' | ');
 
-export default {
-  input: 'src/placekit.js',
-  output: [
-    {
-      file: pkg.module,
-      format: 'es',
-      banner,
-    },
-    {
-      file: pkg.main,
-      format: 'cjs',
-      exports: 'auto',
-      banner,
-    },
-    {
-      file: pkg.browser,
-      format: 'umd',
-      name: 'placekit',
-      banner,
-    },
-  ],
-  plugins: [
-    commonjs(),
-    cleanup(),
-    copy({
-      targets: [
-        {
-          src: 'src/placekit.d.ts',
-          dest: path.dirname(pkg.types),
-          rename: path.basename(pkg.types),
-          transform: (content) => [banner, content].join("\n"),
-        },
-      ]
-    })
-  ],
-};
+export default [
+  {
+    input: [
+      'src/placekit-lite.js',
+      'src/placekit.js',
+    ],
+    output: [
+      {
+        dir: 'dist',
+        format: 'es',
+        banner,
+      },
+      {
+        dir: 'dist',
+        entryFileNames: '[name].cjs',
+        format: 'cjs',
+        exports: 'default',
+        banner,
+      },
+    ],
+    plugins: [
+      cleanup(),
+      copy({
+        targets: [
+          {
+            src: 'src/placekit-lite.d.ts',
+            dest: 'dist',
+            transform: (content) => [banner, content].join("\n"),
+          },
+          {
+            src: 'src/placekit.d.ts',
+            dest: 'dist',
+            transform: (content) => [banner, content].join("\n"),
+          },
+        ]
+      })
+    ],
+  },
+  {
+    input: 'src/placekit-lite.js',
+    output: [
+      {
+        dir: 'dist',
+        entryFileNames: '[name].umd.js',
+        format: 'umd',
+        name: 'placekit',
+        banner,
+      },
+    ],
+    plugins: [
+      cleanup(),
+    ],
+  },
+];
---
+++ src/placekit-lite.d.ts
@@ -0,0 +1,56 @@
+export default PlaceKit;
+export as namespace PlaceKit;
+
+declare function PlaceKit(apiKey?: string, opts?: PKOptions): PKClient;
+
+export type PKClient = {
+  readonly options: PKOptions;
+  readonly hasGeolocation: boolean;
+  search(query: string, opts?: PKOptions): Promise<PKSearchResponse>;
+  reverse(opts?: PKOptions): Promise<PKSearchResponse>;
+  configure(opts?: PKOptions): void;
+  requestGeolocation(opts?: Object): Promise<GeolocationPosition>;
+  clearGeolocation(): void;
+};
+
+type PKType = "airport" | "bus" | "city" | "country" | "street" | "tourism" | "townhall" | "train";
+type PKTypeFilter = PKType | "-airport" | "-bus" | "-city" | "-country" | "-street" | "-tourism" | "-townhall" | "-train";
+
+export type PKOptions = {
+  timeout?: number;
+  maxResults?: number;
+  language?: string;
+  types?: PKTypeFilter[];
+  countries?: string[];
+  countryByIP?: boolean;
+  forwardIP?: string;
+  coordinates?: string;
+};
+
+export type PKResult = {
+  street?: {
+    number: string;
+    suffix: string;
+    name: string;
+  };
+  name: string;
+  city: string;
+  county: string;
+  administrative: string;
+  country: string;
+  countrycode: string;
+  coordinates: string; // "lat,lng"
+  lat?: number; // deprecated
+  lng?: number; // deprecated
+  type: PKType;
+  zipcode: string[];
+  population: number;
+  highlight: string;
+};
+
+export type PKSearchResponse = {
+  results: PKResult[];
+  resultsCount: number;
+  maxResults: number;
+  query: string;
+};
